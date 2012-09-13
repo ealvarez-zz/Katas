86b86bf4bcfc89f9ff8eeb2c06edddf3137d95b7
Python <python@Orion-Linux.(none)>
2012-09-13 20:50:31 -0300

    Add basic generator

--- Game_of_life/game_of_life.py
+++ Game_of_life/game_of_life.py
@@ -1,4 +1,6 @@
-
-
+def game_of_life_generator(seed):
+    while True:
+        yield set()
+    
 
 
--- Game_of_life/game_of_life_tests.py
+++ Game_of_life/game_of_life_tests.py
@@ -1,10 +1,17 @@
 import unittest
+from game_of_life import game_of_life_generator
 
-class generatorTest(unittest.TestCase):
+class GeneratorTest(unittest.TestCase):
     
-    pass
-    
-    
+    def test_generator(self):
+        seed = set()
+        game = game_of_life_generator(seed)
+        
+        self.assertEqual(game.next(), set())
+        
+        
+        
+        
         
 
 
