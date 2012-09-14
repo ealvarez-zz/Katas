5d5d3934187bcd3dd71e894d8c8f07a85a0e2010
Python <python@Orion-Linux.(none)>
2012-09-13 21:12:51 -0300

    Add GameOfLife class

--- Game_of_life/game_of_life.py
+++ Game_of_life/game_of_life.py
@@ -1,6 +1,16 @@
 def game_of_life_generator(seed):
+    
+    game = GameOfLife(seed)
+    
     while True:
-        yield set()
+        yield game.tick()
     
 
 
+class GameOfLife(object):
+    def __init__(self, seed):
+        self.alive_cells = seed
+        
+        
+    def tick(self):
+        return set()
--- Game_of_life/game_of_life_tests.py
+++ Game_of_life/game_of_life_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from game_of_life import game_of_life_generator
+from game_of_life import game_of_life_generator, GameOfLife
 
 class GeneratorTest(unittest.TestCase):
     
@@ -10,9 +10,37 @@ class GeneratorTest(unittest.TestCase):
         self.assertEqual(game.next(), set())
         
         
+    def test_blinker_several_generations(self):
         
+        """
+        ...
+        ***
+        ...
         
+        ->
         
+        .*.
+        .*.
+        .*.   
+
+        """
+        
+        seed = set([(0,1),(1,1),(2,1)])
+        
+        game = game_of_life_generator(seed)
+        
+        self.assertEqual(game.next(), set([(1,0),(1,1),(1,2)]))
+        self.assertEqual(game.next(), set([(0,1),(1,1),(2,1)]))
+        
+
+class GameOfLifeTest(unittest.TestCase):
+    
+    def test_tick(self):
+        
+        seed = set()
+        game = GameOfLife(seed)
+        
+        self.assertEqual(game.tick(), set())
 
 
 
